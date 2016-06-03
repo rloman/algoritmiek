554759866345c51d4c28b6d293360c1e39498fd4
Carpago Software | Raymond Loman <raymond@carpago.nl>
2016-06-03 22:52:46 +0200

    Fix a broken unittest

--- src/main/java/eu/programit/Gauss.java
+++ src/main/java/eu/programit/Gauss.java
@@ -1,27 +1,13 @@
 package eu.programit;
 
-import java.util.List;
-
 public class Gauss {
 	
-	List<Integer> l;
-
 	public int gauss(int n) {
-		l.add(new Integer(3));
 		return (n*(n+1)) / 2;
 	}
 
 	public int graph(int n) {
 		return (n*(n-1)) / 2;
 	}
-	
-	public Person getPerson() {
-		Person p = null;
-		return p;
-	}
-
-}
-
-class Person {
 	
 }
